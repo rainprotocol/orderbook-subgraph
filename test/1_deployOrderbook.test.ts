6e36675e3be64dbbbe2d236c1fcf008af8e0e4b3
NanezX <victor.nanezj@gmail.com>
2023-03-28 23:09:05 -0400

    modularize tests

--- test/1_deployOrderbook.test.ts
+++ test/1_deployOrderbook.test.ts
@@ -1,70 +1,10 @@
-import { ethers } from "hardhat";
-import deploy1820 from "../utils/deploy/registry1820/deploy";
-import {
-  exec,
-  fetchFile,
-  fetchSubgraph,
-  waitForGraphNode,
-  waitForSubgraphToBeSynced,
-  writeFile,
-} from "./utils";
-import { DeployerDiscoverableMetaV1ConstructionConfigStruct } from "../typechain/contracts/factory/CloneFactory";
-import {
-  assertError,
-  getRainMetaDocumentFromContract,
-  zeroAddress,
-} from "../utils";
-import { getTouchDeployer } from "../utils/deploy/interpreter/shared/rainterpreterExpressionDeployer/deploy";
-import { OrderBook } from "../typechain";
-import assert from "assert";
-import * as path from "path";
-import { deployOrderBook } from "../utils/deploy/orderBook/deploy";
-import { ApolloFetch, FetchResult } from "apollo-fetch";
+import { waitForSubgraphToBeSynced } from "./utils";
 
-export let orderBook: OrderBook;
-export let subgraph: ApolloFetch;
+import assert from "assert";
+import { FetchResult } from "apollo-fetch";
+import { orderBook, subgraph } from "./0_initialization.test";
 
 describe("Orderbook deploy test", () => {
-  before(async () => {
-    await waitForGraphNode();
-    const signers = await ethers.getSigners();
-    await deploy1820(signers[0]);
-
-    const orderBookFactory = await ethers.getContractFactory("OrderBook", {});
-    const touchDeployer = await getTouchDeployer();
-
-    const config0: DeployerDiscoverableMetaV1ConstructionConfigStruct = {
-      meta: getRainMetaDocumentFromContract("orderbook"),
-      deployer: touchDeployer.address,
-    };
-
-    orderBook = await deployOrderBook();
-
-    const configPath = path.resolve(__dirname, "../config/localhost.json");
-
-    assert(!(orderBook.address === zeroAddress), "OrderBook did not deploy");
-
-    const config = JSON.parse(fetchFile(configPath));
-
-    config.network = "localhost";
-    config.orderbook = orderBook.address;
-    config.blockNumber = orderBook.deployTransaction.blockNumber;
-
-    writeFile(configPath, JSON.stringify(config, null, 2));
-
-    // create subgraph instance
-    exec("graph create --node http://localhost:8020/ test/test");
-    // prepare subgraph manifest
-    exec(
-      "npx mustache config/localhost.json subgraph.template.yaml subgraph.yaml"
-    );
-    // deploy subgraph
-    exec(
-      "graph deploy --node http://localhost:8020/ --ipfs http://localhost:5001 test/test  --version-label 1"
-    );
-    subgraph = fetchSubgraph("test/test");
-  });
-
   it("Should create orderBook entity", async () => {
     await waitForSubgraphToBeSynced();
     const query = `{
