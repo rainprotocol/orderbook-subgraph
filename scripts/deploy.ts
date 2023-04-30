8ee1d54e2dd72f9453451c6594847e3374035da4
vishal <vishalkale151071@gmail.com>
2023-04-30 14:07:52 +0530

    added etherscan check on contract ABI

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,21 +3,16 @@ import * as dotenv from "dotenv";
 import * as path from "path";
 import * as fs from "fs";
 import { program } from "commander";
-import { artifacts, ethers, network } from "hardhat";
+import { artifacts, ethers } from "hardhat";
 import { FetchResult, createApolloFetch } from "apollo-fetch";
-import { getRainMetaDocumentFromContract, inflateJson } from "../utils";
+import { inflateJson } from "../utils";
 import { decodeRainMetaDocument } from "../utils/meta/cbor";
 import { MAGIC_NUMBERS } from "../test/utils";
+import "colors";
+import { exit } from "process";
 
 dotenv.config();
 
-interface DeployConfig {
-  configPath: string;
-  subgraphName: string;
-  versionLabel: string;
-  endpoint: string;
-  ipfsEndpoint: string;
-}
 
 /**
  * Execute Child Processes
@@ -52,9 +47,8 @@ const getRPCURL = (network: string): string => {
 
 const checkContract = async (address: string, network: string) => {
   const provider = new ethers.providers.JsonRpcProvider(getRPCURL(network));
-
   const code = await provider.getCode(address);
-  if(code.length < 3) throw new Error("Contract address does not have any bytecode.");
+  if(code.length < 3) throw new Error("Contract address does not have any bytecode.".red);
 }
 
 const getSubgraph = (network: string): string => {
@@ -74,9 +68,9 @@ const getABIPayload = (meta: any[]): any => {
   }
 }
 
-const checkMeta = async (network: string, contract: string): Promise<number> => {
+const checkMetaFromSubgraph = async (network: string, contract: string): Promise<number> => {
   const subgraphEndpoint = getSubgraph(network);
-  const supgraph = createApolloFetch({uri: subgraphEndpoint});
+  const subgraph = createApolloFetch({uri: subgraphEndpoint});
 
   const query = `{
     contract(id: "${contract.toLowerCase()}") {
@@ -89,14 +83,27 @@ const checkMeta = async (network: string, contract: string): Promise<number> => 
     }
   }`;
 
-  const response = (await supgraph({query})) as FetchResult;
+  const response = (await subgraph({query})) as FetchResult;
   const data = response.data.contract;
-  if (data == null) throw new Error(`Contract ${contract} not available on ${network}`);
+  if (data == null) {
+    console.log(`Contract ${contract} is not available on ${network} registry subgraph`.yellow);
+    return -1;
+  }
   
   const decoded = decodeRainMetaDocument(data.meta.metaBytes)
   const onchainABI = JSON.parse(inflateJson(getABIPayload(decoded)));
-  const localABI = artifacts.readArtifactSync("OrderBook").abi;
   
+  if(checkABI(onchainABI)){
+    console.log("Contract ABI Matches with Local ABI".green);
+    // return data.deployTransaction.blockNumber;
+    return -1;
+  }else{
+    throw new Error("Contract ABI doesn't match with onchain ABI".red);
+  }
+}
+
+const checkABI = (onchainABI: any): boolean => {
+  const localABI = artifacts.readArtifactSync("OrderBook").abi;
   let onchainEvents = [];
   let localEvents = [];
   
@@ -115,24 +122,22 @@ const checkMeta = async (network: string, contract: string): Promise<number> => 
   }
 
   if (JSON.stringify(onchainEvents) == JSON.stringify(localEvents)){
-    console.log("Contract ABI Matches with Local ABI");
-    return data.deployTransaction.blockNumber
+    return true;
   } else {
-    throw new Error("Contract ABI doesn't match with onchain ABI");
+    return false;
   }
 }
-
 
 const main = async () => {
   program
     .requiredOption("--contractAddress <string>", "Smart contract address")
     .requiredOption(
       "--network <string>",
-      "Block Number to start indexing from.",
+      "name of the network",
     )
     .requiredOption(
       "--subgraphName <string>",
-      "The subgraph name to deploy. Eg: 'user/name'."
+      "The subgraph name to deploy. Eg: 'username/subgraphName'."
     )
     .requiredOption(
       "--graphAccessToken <string>",
@@ -142,6 +147,14 @@ const main = async () => {
       "--subgraphTemplate <string>",
       "Specify a path to yaml file to be used as template. By the default use the root template.",
       "subgraph.template.yaml"
+    )
+    .option(
+      "--blockNumber <string>",
+      "block number of contract creation",
+    )
+    .option(
+      "--etherscanAPIKey <string>",
+      "etherscanAPIKey for selected network",
     );
 
   program.parse();
@@ -153,13 +166,22 @@ const main = async () => {
 
   const _network = options.network;
   const _contractAddress = options.contractAddress;
-  const _blockNumber = await checkMeta(_network, _contractAddress);
+  const _blockNumber = await checkMetaFromSubgraph(_network, _contractAddress);
   const _graphAccessToken = options.graphAccessToken;
   const _subgraphName = options.subgraphName;
   const _endpoint = "--node https://api.thegraph.com/deploy/";
   const _subgraphTemplate = options.subgraphTemplate;
+  const _api_key = options.etherscanAPIKey;
 
   // Add the address to the subgraph.yaml file
+  if (_blockNumber < 0) {
+    if (_api_key == null) throw new Error("please try again with passing --etherscanAPIKey and --blocknumber options".cyan);
+    const api = require("etherscan-api").init(_api_key);
+    const sourceCode = await api.contract.getsourcecode(_contractAddress)
+    const onchainABI = JSON.parse(sourceCode.result[0].ABI)
+    if (checkABI(onchainABI)) console.log("Contract ABI Matches with Local ABI".green) 
+    else throw new Error("Contract ABI doesn't match with onchain ABI".red);
+  }
 
   exec(`npx graph auth --product hosted-service ${_graphAccessToken}`)
 
